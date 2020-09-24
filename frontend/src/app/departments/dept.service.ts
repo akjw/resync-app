a1602b60a853d13cf9c833fa26445d4dd7f5c09b
akjw <aseykoh@gmail.com>
2020-09-24 12:00:01 +0800

    rm old dist

--- frontend/src/app/departments/dept.service.ts
+++ frontend/src/app/departments/dept.service.ts
@@ -19,51 +19,51 @@ export class DeptService {
   constructor(private http: HttpClient) { }
 
   getDepartments() {
-    return this.http.get<any>(`api/departments`,  { headers: {
+    return this.http.get<any>(`${URL}api/departments`,  { headers: {
       "x-auth-token": this.token,
     }})
   }
 
   getMonthDepts() {
-    return this.http.get<any>(`api/departments/month`,  { headers: {
+    return this.http.get<any>(`${URL}api/departments/month`,  { headers: {
       "x-auth-token": this.token,
     }})
   }
 
   getYearDepts() {
-    return this.http.get<any>(`api/departments/year`,  { headers: {
+    return this.http.get<any>(`${URL}api/departments/year`,  { headers: {
       "x-auth-token": this.token,
     }})
   }
 
   getOrgDepts(id: string){
-    return this.http.get<any>(`api/departments/organization/${id}`,  { headers: {
+    return this.http.get<any>(`${URL}api/departments/organization/${id}`,  { headers: {
       "x-auth-token": this.token,
     }})
   }
 
   addDept(info: DeptInfo){
     return this.http
-    .post<any>(`api/departments/new`, info, { headers: {
+    .post<any>(`${URL}api/departments/new`, info, { headers: {
       "x-auth-token": this.token,
     }})
   }
 
   getDept(id: string){
-    return this.http.get<any>(`api/departments/${id}`,  { headers: {
+    return this.http.get<any>(`${URL}api/departments/${id}`,  { headers: {
       "x-auth-token": this.token,
     }})
   }
 
   editDept(id: string, info: any){
     return this.http
-    .put<any>(`api/departments/${id}`, info, { headers: {
+    .put<any>(`${URL}api/departments/${id}`, info, { headers: {
       "x-auth-token": this.token,
     }})
   }
 
   deleteDept(id: string){
-    return this.http.delete<any>(`api/departments/${id}`,  { headers: {
+    return this.http.delete<any>(`${URL}api/departments/${id}`,  { headers: {
       "x-auth-token": this.token,
     }})
   }
